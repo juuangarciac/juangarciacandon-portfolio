10112677870c5b58a61a15cb78fee2a1d9e1f9a5
Juan Garcia Candon <113298385+juuangarciac@users.noreply.github.com>
2024-04-23 16:59:55 +0200

    Update home

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,9 +1,96 @@
-import React from "react";
+import React, { useEffect } from "react";
+import GeneralCompetencesLogo from "../img/LogoCard_CompetenciaGeneral_es.png";
+import SpecificCompetencesLogo from "../img/LogoCard_CompetenciaEspecifica_es.png";
+import HomeBackground from "../img/home_background.png";
+import { Competences } from "../data/Competences";
+import { useState } from "react";
+
 
 export const Inicio = () => {
+
+  const [userName, setUserName] = useState("");
+
+  const [generalCompetences, setgeneralCompetences] = useState([]);
+  const [specificCompetences, setspecificCompetences] = useState([]);
+
+  const filterDataByCompetence = (name) => {
+    return Competences.filter(competence => competence.name === name);
+  }
+
+  const updateUserName = (e) => {
+    setUserName(e.target.value);
+    console.log(e.target.value);
+  }
+
+  useEffect(() => {
+    setgeneralCompetences(filterDataByCompetence("GeneralCompetence"));
+    setspecificCompetences(filterDataByCompetence("SpecificCompetence"));
+  }, []);
+
   return (
-    <div className="row justify-content-center">
-      
+    <div className="bg-dark text-light">
+      <div className="card text-center w-100">
+        <img className="img" src={HomeBackground} alt="Color azul degradado" />
+        <div class="card-img-overlay">
+          <h1 class="card-title"><strong> Inicio </strong></h1>
+          <div class="form-group justify-content-center">
+            <input type="text" name="username" id="username" class="form-control w-25 " placeholder="Introduzca su nombre..." onChange={updateUserName}/>
+          </div> 
+          <h1 class="card-title">Bienvenido a mi página web <strong>{userName}</strong></h1>
+          <a href="SobreMi" class="btn btn-primary">Sobre mi</a>
+        </div>
+      </div>
+
+
+      <div className="row" id="SobreMi">
+        <h2 className="h2"> Sobre mi </h2>
+      </div>
+
+
+      <h2 className="h2"> Competencias </h2>
+      <div className="row justify-content-center" id="Competencias">
+          < h3 className="h3"> Competencias Generales</h3>
+          {
+            generalCompetences ? (
+              generalCompetences.map(competence => 
+                <div key={competence.id} className="card text-dark bg-light col-2 m-4">
+                    <div class="card-body">
+                    <img
+                      src={GeneralCompetencesLogo}
+                      className="card-img-top"
+                      alt={"Logo Competencia General"}
+                    ></img>
+                      <p class="card-text">{competence.description}</p>
+                    </div>
+                </div>
+              )
+            ) : (
+              <p>Cargando...</p>
+            )
+          }
+
+          < h3 className="h3"> Competencias Específicas</h3>
+          {
+            specificCompetences ? (
+              specificCompetences.map(competence => 
+                <div key={competence.id} className="card text-dark bg-light col-2 m-4">
+                    <div class="card-body">
+                    <img
+                      src={SpecificCompetencesLogo}
+                      className="card-img-top"
+                      alt={"Logo Competencia General"}
+                    ></img>
+                      <p class="card-text">{competence.description}</p>
+                    </div>
+                </div>
+              )
+            ) : (
+              <p>Cargando...</p>
+            )
+          }
+          < h3 className="h3"> Competencias de Tecnología de Información</h3>
+      </div>
+
     </div>
   );
 };
--- src/data/Competences.js
+++ src/data/Competences.js
@@ -3,56 +3,57 @@ export const Competences = [
     id: "0",
     name: "GeneralCompetence",
     description:
-      "Capacidad para diseñar, desarrollar, evaluar y asegurar la accesibilidad, ergonomía, usabilidad y seguridad de los sistemas, servicios y aplicaciones informáticas, así como de la información que gestionan.",
+      "Diseñar, desarrollar, evaluar, servicios y aplicaciones informáticas, así como de la información que gestionan.",
   },
   {
     id: "1",
     name: "GeneralCompetence",
     description:
-      "Conocimiento de las materias básicas y tecnologías, que capaciten para el aprendizaje y desarrollo de nuevos métodos y tecnologías, así como las que les doten de una gran versatilidad para adaptarse a nuevas situaciones.",
+      "Aprendizaje y desarrollo de nuevos métodos y tecnologías.",
   },
   {
     id: "2",
     name: "GeneralCompetence",
     description:
-      "Capacidad para resolver problemas con iniciativa, toma de decisiones, autonomía y creatividad.  Capacidad para saber comunicar y transmitir los conocimientos, habilidades y destrezas de la profesión de Ingeniero Técnico en Informática.",
+      "Resolver problemas con iniciativa, toma de decisiones, autonomía y creatividad.",
   },
   {
     id: "3",
     name: "GeneralCompetence",
     description:
-      "Capacidad para la resolución de los problemas matemáticos que puedan plantearse en la ingeniería. Aptitud para aplicar los conocimientos sobre: álgebra lineal; cálculo diferencial e integral; métodos numéricos; algorítmica numérica; estadística y optimización.",
+      "Saber comunicar y transmitir los conocimientos, habilidades y destrezas.",
   },
   {
     id: "4",
     name: "GeneralCompetence",
     description:
-      "Conocimiento de la estructura, organización, funcionamiento e interconexión de los sistemas informáticos, los fundamentos de su programación, y su aplicación para la resolución de problemas propios de la ingeniería.",
+      "Resolución de los problemas matemáticos que puedan plantearse en la ingeniería.",
   },
   {
     id: "5",
     name: "GeneralCompetence",
-    description: "Capacidad para analizar y valorar el impacto social y medioambiental de las soluciones técnicas, comprendiendo la responsabilidad ética y profesional de la actividad del Ingeniero Técnico en Informática."
+    description:
+      "Conocimiento de la estructura, organización, funcionamiento e interconexión de los sistemas informáticos.",
   },
   {
     id: "6",
-    name: "SpecificCompetence",
-    description: "Capacidad para diseñar, desarrollar, seleccionar y evaluar aplicaciones y sistemas informáticos, asegurando su fiabilidad, seguridad y calidad, conforme a principios éticos y a la legislación y normativa vigente."
+    name: "GeneralCompetence",
+    description: "Analizar y valorar el impacto social y medioambiental de las soluciones técnicas."
   },
   {
     id: "7",
     name: "SpecificCompetence",
-    description: "Capacidad para comprender la importancia de la negociación, los hábitos de trabajo efectivos, el liderazgo y las habilidades de comunicación en todos los entornos de desarrollo de software."
+    description: "Capacidad para diseñar, desarrollar, seleccionar y evaluar aplicaciones y sistemas informáticos, asegurando su fiabilidad, seguridad y calidad, conforme a principios éticos y a la legislación y normativa vigente."
   },
   {
     id: "8",
     name: "SpecificCompetence",
-    description: "Conocimiento, administración y mantenimiento sistemas, servicios y aplicaciones informáticas."
+    description: "Capacidad para comprender la importancia de la negociación, los hábitos de trabajo efectivos, el liderazgo y las habilidades de comunicación en todos los entornos de desarrollo de software."
   },
   {
     id: "9",
     name: "SpecificCompetence",
-    description: "Conocimiento, diseño y utilización de forma eficiente los tipos y estructuras de datos más adecuados a la resolución de un problema."
+    description: "Conocimiento, administración y mantenimiento sistemas, servicios y aplicaciones informáticas."
   },
   {
     id: "10",
@@ -62,10 +63,15 @@ export const Competences = [
   {
     id: "11",
     name: "SpecificCompetence",
-    description: "	Conocimiento y aplicación de los principios, metodologías y ciclos de vida de la ingeniería de software."
+    description: "Conocimiento, diseño y utilización de forma eficiente los tipos y estructuras de datos más adecuados a la resolución de un problema."
   },
   {
     id: "12",
+    name: "SpecificCompetence",
+    description: "	Conocimiento y aplicación de los principios, metodologías y ciclos de vida de la ingeniería de software."
+  },
+  {
+    id: "13",
     name: "SpecificCompetence",
     description : "Capacidad para diseñar y evaluar interfaces persona computador que garanticen la accesibilidad y usabilidad a los sistemas, servicios y aplicaciones informáticas."
   }
